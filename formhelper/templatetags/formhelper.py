d3adce5fc7567bab0a0c111e2564491d5f84515c
Ben Davis <bendavis78@gmail.com>
2014-04-25 11:10:31 -0500

    pep8 cleanup

--- formhelper/templatetags/formhelper.py
+++ formhelper/templatetags/formhelper.py
@@ -7,6 +7,7 @@ register = template.Library()
 first_cap_re = re.compile('(.)([A-Z][a-z]+)')
 all_cap_re = re.compile('([a-z0-9])([A-Z])')
 
+
 @register.filter
 def field_error(field):
     if field and field.errors:
@@ -18,20 +19,24 @@ def field_error(field):
         return err
     return ''
 
+
 @register.filter
 def error_has_is_required(field):
     err = field_error(field)
     return ' is required' in err
 
+
 @register.filter
 def has_is_required_errors(form):
     if form.errors:
-        return any(error_has_is_required(form.__getitem__(f)) for f in form.errors.keys())
+        return any(error_has_is_required(form.__getitem__(f))
+                   for f in form.errors.keys())
     return False
+
 
 @register.filter
 def class_names(field):
-    class_names = [field.name.replace('_','-')]
+    class_names = [field.name.replace('_', '-')]
     widget_class = pyclass_to_cssclass(field.field.widget.__class__.__name__)
     class_names.append(widget_class)
     if field.field.required:
@@ -39,6 +44,7 @@ def class_names(field):
     if field.errors:
         class_names.append('error')
     return ' '.join(class_names)
+
 
 class FormFieldNode(template.Node):
     def __init__(self, form, field):
@@ -51,7 +57,9 @@ class FormFieldNode(template.Node):
         try:
             form = self.form.resolve(context)
         except template.VariableDoesNotExist:
-            raise template.TemplateSyntaxError('a form must either be passed to form_field as first argument or must exist in context as "form"')
+            raise template.TemplateSyntaxError(
+                'a form must either be passed to form_field as first argument '
+                'or must exist in context as "form"')
         if not form:
             raise template.TemplateSyntaxError('form is empty')
         tpl = get_template('formhelper/includes/field.html')
@@ -60,15 +68,18 @@ class FormFieldNode(template.Node):
         html = tpl.render(context)
         context.pop()
         return html
-        
+
+
 @register.tag
 def form_field(parser, token):
     args = token.split_contents()[1:]
     if not args:
-        raise template.TemplateSyntaxError("form_field takes at least one argument")
+        raise template.TemplateSyntaxError("form_field takes at least one "
+                                           "argument")
     if len(args) == 1:
         return FormFieldNode(None, args[0])
     return FormFieldNode(args[0], args[1])
+
 
 class FormRowNode(template.Node):
     def __init__(self, args):
@@ -76,34 +87,41 @@ class FormRowNode(template.Node):
 
     def render(self, context):
         tpl = get_template('formhelper/includes/form_row.html')
-        if context.get(self.args[0]) and isinstance(context[self.args[0]], forms.Form):
+        if context.get(self.args[0]) and isinstance(context[self.args[0]],
+                                                    forms.Form):
             form = context[self.args[0]]
             field_list = self.args[1:]
         elif context.get('form'):
             form = context['form']
             field_list = self.args
         else:
-            raise template.TemplateSyntaxError('a form must either be passed to form_row as first argument or must exist in context as "form"')
+            raise template.TemplateSyntaxError(
+                'a form must either be passed to form_row as first argument '
+                'or must exist in context as "form"')
         fields = [_get_field(f, form, context) for f in field_list]
         context.update({'form': form, 'fields': fields})
         html = tpl.render(context)
         context.pop()
         return html
-        
+
+
 @register.tag
 def form_row(parser, token):
     args = token.split_contents()[1:]
     if not args:
-        raise template.TemplateSyntaxError("form_row takes at least one argument")
+        raise template.TemplateSyntaxError(
+            "form_row takes at least one argument")
     return FormRowNode(args)
+
 
 @register.inclusion_tag('formhelper/includes/errorlist.html')
 def error_list(form, only='', suppress_is_required=0):
     return {
-        'form':form,
-        'only':only,
-        'suppress_is_required':suppress_is_required
+        'form': form,
+        'only': only,
+        'suppress_is_required': suppress_is_required
     }
+
 
 @register.filter
 def field_value(field):
@@ -115,6 +133,7 @@ def field_value(field):
     except TypeError:
         return None
 
+
 @register.filter
 def formset_verbose_name(formset):
     if hasattr(formset, 'verbose_name'):
@@ -122,6 +141,7 @@ def formset_verbose_name(formset):
     if hasattr(formset, 'model'):
         return formset.model._meta.verbose_name
     return ''
+
 
 @register.filter
 def formset_verbose_name_plural(formset):
@@ -139,6 +159,7 @@ def is_empty_form(form):
     """
     return form.prefix.endswith('__prefix__')
 
+
 @register.simple_tag(takes_context=True)
 def formset_opts(context, formset):
     import json
@@ -147,6 +168,7 @@ def formset_opts(context, formset):
     if opts:
         formset_opts = opts.get(formset.prefix, {})
     return json.dumps(formset_opts)
+
 
 def _get_field(field, form, context):
     try:
@@ -157,7 +179,7 @@ def _get_field(field, form, context):
         field = form[field]
     return field
 
+
 def pyclass_to_cssclass(name):
     s1 = first_cap_re.sub(r'\1-\2', name)
     return all_cap_re.sub(r'\1-\2', s1).lower()
-
--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
+import os
+
+dir = os.path.dirname(__file__)
+readme = os.path.join(dir, 'README.rst')
 
 setup(
-    name='formhelper', 
+    name='django-formhelper',
     version='0.1',
     author='Ben Davis',
-    author_email='bendavis78@gmail.com',
-    url='http://github.com/bendavis78',
-    description='Django FormHelper is a collection of templates and templatetags to ease the pain in building out web forms',
+    author_email='code@bendavismedia.com',
+    url='http://github.com/bendavis78/django-form-helper',
+    description=('Django FormHelper is a collection of templates and '
+                 'templatetags to ease the pain in building out web forms'),
     keywords='django forms',
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
@@ -16,5 +21,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     packages = find_packages(),
-    include_package_data = True
+    include_package_data = True,
+    log_description=open(readme).read()
 )
