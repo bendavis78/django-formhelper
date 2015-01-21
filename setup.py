576c2ff805c69603e3ce0f55c27e5be355692d50
Ben Davis <bendavis78@gmail.com>
2015-01-21 17:51:22 -0600

    fixed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='formhelper',
+    name='django-formhelper',
     version='0.2',
     author='Ben Davis',
     author_email='bendavis78@gmail.com',
